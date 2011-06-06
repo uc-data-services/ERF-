37ba8d4da34fc0d5f88ac22ee78548278e852ea9
Tim Dennis <dennis.tim@gmail.com>
2011-06-05 18:12:10 -0700

    adding files

---
+++ erf.py
@@ -0,0 +1,46 @@
+import urllib2
+import re
+import sqlite3
+
+"""
+1.access by type erf and findall resourcetypes
+2.open each resourceid pages and findall resourceIDs, we also will need to persist these ids in DB, but might
+ be part of record page
+3. open each http://cluster4.lib.berkeley.edu:8080/ERF/servlet/ERFmain?cmd=detail&resId=[#]
+and then parse html into sqlite3 db - need to model db. am assuming will create a dictionary
+for this. will need to keep up what resId are already saved to db and use logic not to add
+duplicates
+"""
+#parts of the ERF urls for global use
+baseurl = 'http://cluster4.lib.berkeley.edu:8080/ERF/servlet/ERFmain?'
+all_res_types = 'cmd=allResTypes'
+search_res_types = 'cmd=searchResType&'
+detail = 'cmd=detail'
+
+#open each resource 'detail' page in erf & do something!
+res_ids = get_resource_ids() 
+
+for id in res_ids:
+    
+
+#function that returns a unique set of ERF resource ids
+def get_resource_ids():
+ 
+    # Open erfby type page & pull out all resTypeId=\d+ as array
+    response = urllib2.urlopen(baseurl+all_res_types)
+    html = response.read()
+    restypeid = re.findall('resTypeId=\d+', html)
+    
+    resids = []
+    
+    #Open each resTypeId page & capture the indiviual ERF resource ids (resIds)
+    for id in restypeid:
+        typeurl = baseurl + search_res_types + str(id)
+        typeresponse = urllib2.urlopen(typeurl)
+        typehtml = typeresponse.read()
+        resid_part = re.findall('resId=\d+', typehtml)
+        resids.extend(resid_part)
+
+    unique_resids = set(resids)
+
+    return(unique_resids)
