2cef6176db3df73a5b1178f03128e62c0ad0411f
Tim Dennis <dennis.tim@gmail.com>
2011-08-04 14:39:53 -0700

    changing table name

--- erf_schema.sql
+++ erf_schema.sql
@@ -8,6 +8,6 @@ CREATE TABLE "r_s_bridge" ("rid" INTEGER NOT NULL , "sid" INTEGER NOT NULL , "is
 CREATE TABLE "r_t_bridge" ("rid" INTEGER NOT NULL , "tid" INTEGER NOT NULL , PRIMARY KEY ("rid", "tid"));
 CREATE TABLE "resource" ("rid" INTEGER PRIMARY KEY  NOT NULL, "title" TEXT,"resource_id" INTEGER,"text" TEXT DEFAULT (NULL),
                 "description" TEXT,"coverage" TEXT DEFAULT (NULL), "licensing" TEXT,"last_modified" DATETIME,"url" TEXT,
-                "alternative_title" TEXT DEFAULT (NULL));             
+                "alternate_title" TEXT DEFAULT (NULL));             
 CREATE TABLE "subject" ("sid" INTEGER PRIMARY KEY  NOT NULL , "term" TEXT UNIQUE );
 CREATE TABLE "type" ("tid" INTEGER PRIMARY KEY  NOT NULL , "type" TEXT UNIQUE);
