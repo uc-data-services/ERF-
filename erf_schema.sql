110a0512bc70772413fd254fed694ea75f83b65e
Tim Dennis <dennis.tim@gmail.com>
2011-09-17 13:33:16 -0700

    fixing sql

--- erf_schema.sql
+++ erf_schema.sql
@@ -5,11 +5,38 @@ DROP TABLE IF EXISTS subject;
 DROP TABLE IF EXISTS type;
 DROP TABLE IF EXISTS alternate_title;
 
-CREATE TABLE "r_s_bridge" ("rid" INTEGER NOT NULL,"sid" INTEGER NOT NULL,"is_core" BOOL DEFAULT (0), PRIMARY KEY ("rid","sid"))
-CREATE TABLE "r_t_bridge" ("rid" INTEGER NOT NULL , "tid" INTEGER NOT NULL , PRIMARY KEY ("rid", "tid"));
-CREATE TABLE "resource" ("rid" INTEGER PRIMARY KEY  NOT NULL, "title" TEXT,"resource_id" INTEGER,"text" TEXT DEFAULT (NULL),
-                "description" TEXT,"coverage" TEXT DEFAULT (NULL), "licensing" TEXT,"last_modified" DATETIME,"url" TEXT, 
-                "is_canceled" BOOL DEFAULT (0));             
-CREATE TABLE "subject" ("sid" INTEGER PRIMARY KEY  NOT NULL , "term" TEXT UNIQUE );
-CREATE TABLE "type" ("tid" INTEGER PRIMARY KEY  NOT NULL , "type" TEXT UNIQUE);
-CREATE TABLE "alternate_title" ("aid" INTEGER PRIMARY KEY  NOT NULL , "title" TEXT DEFAULT NULL, "rid" INTEGER);
+CREATE TABLE r_s_bridge (
+    rid INTEGER NOT NULL,
+    sid INTEGER NOT NULL,
+    is_core BOOL DEFAULT (0), 
+    PRIMARY KEY (rid,sid))
+
+CREATE TABLE r_t_bridge (
+    rid INTEGER NOT NULL , 
+    tid INTEGER NOT NULL , 
+    PRIMARY KEY (rid, tid));
+
+CREATE TABLE resource (
+    rid INTEGER PRIMARY KEY NOT NULL, 
+    title TEXT,
+    resource_id INTEGER,
+    text TEXT DEFAULT (NULL),
+    description TEXT,
+    coverage TEXT DEFAULT (NULL), 
+    licensing TEXT,
+    last_modified DATETIME,
+    url TEXT, 
+    is_canceled BOOL DEFAULT (0));
+
+CREATE TABLE subject (
+    sid INTEGER PRIMARY KEY NOT NULL , 
+    term TEXT UNIQUE );
+
+CREATE TABLE type (
+    tid INTEGER PRIMARY KEY NOT NULL , 
+    type TEXT UNIQUE);
+
+CREATE TABLE alternate_title (
+    aid INTEGER PRIMARY KEY  NOT NULL, 
+    title TEXT DEFAULT NULL, 
+    rid INTEGER);
